46e9e7af39ca8d7cd29c850ce988c1a5414fcfd3
Luiz Eduardo <luiz.lr.2010@gmail.com>
2022-03-15 15:12:42 -0300

    Adding libs and configuring App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { GlobalStyles } from 'twin.macro';
 
-function App() {
-  return <div className="App"></div>;
+import StylesBase from './styles/global';
+
+const App: React.FC = () =>{
+  return (
+    <Router>
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
+      <StylesBase />
+      <GlobalStyles />
+    </Router>
+  )
 }
-
-export default App;
