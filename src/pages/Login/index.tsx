875d73d579fb06bcca8f878d6ee685a61be9c5f1
Luiz Eduardo <luiz.lr.2010@gmail.com>
2022-03-16 10:40:40 -0300

    Create page new user

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,60 +1,73 @@
-import React from "react"
+import React from 'react';
 import 'twin.macro';
-
-import image from '../../assets/Login/login.png'
-import Button from "../../components/Button"
-import { ContainerLogin } from './styles'
-
 import { useForm } from 'react-hook-form';
 
-interface ILogin {
-    email: string;
-    password: string;
-}
+import image from '../../assets/Login/login.png';
+import Button from '../../components/Button';
+import { ContainerLogin } from './styles';
 
+interface ILogin {
+  email: string;
+  password: string;
+}
 
 export const Login: React.FC = () => {
-    const { register, handleSubmit } = useForm();
+  const { register, handleSubmit } = useForm();
 
-    const onSubmit = async (inputData: ILogin) => {
-        const data = {
-            user: inputData.email,
-            password: inputData.password
-        }
-        console.log(data)
+  const onSubmit = async (inputData: ILogin) => {
+    const data = {
+      user: inputData.email,
+      password: inputData.password,
     };
+    console.log(data);
+  };
 
-    return (
-        <ContainerLogin>
-            <img src={image} alt="" />
-            <div className="content">
-                <h1>Faça login no <span>painel</span>
-                </h1>
-                <form action="" onSubmit={handleSubmit(onSubmit)}>
-                    <div className="boxInput">
-                        <label htmlFor="">Nome de Usuário ou Endereço de Email</label>
-                        <input id="email" type="text" placeholder="Digite o seu usuário ou email"  {...register('email', { required: true })}/>
-                    </div>
-                    <div className="boxInput">
-                        <label htmlFor="">Senha</label>
-                        <input id="password" type="password" placeholder="Digite a sua senha"  {...register('password', { required: true })}/>
-                    </div>
-                    <div className="radio">
-                        <label>
-                            <input type="checkbox" name="sfg" />
-                            <div>Mantenha-me conectado</div>
-                        </label>
-                        <div className="password">
-                           <a href="https://github.com/Luizrtr/"><strong>Esqueceu minha senha?</strong></a>
-                        </div>
-                    </div>
-                    <div>
-                        <Button tw="w-full md:w-5/12 text-center" type="submit">Entrar</Button>
-                    </div>
-                </form>
+  return (
+    <ContainerLogin>
+      <img src={image} alt="" />
+      <div className="content">
+        <h1>
+          Faça login no <span>painel</span>
+        </h1>
+        <form  onSubmit={handleSubmit(onSubmit)}>
+          <div className="boxInput">
+            <label htmlFor="">Nome de Usuário ou Endereço de Email</label>
+            <input
+              id="email"
+              type="text"
+              placeholder="Digite o seu usuário ou email"
+              {...register('email', { required: true })}
+            />
+          </div>
+          <div className="boxInput">
+            <label htmlFor="">Senha</label>
+            <input
+              id="password"
+              type="password"
+              placeholder="Digite a sua senha"
+              {...register('password', { required: true })}
+            />
+          </div>
+          <div className="radio">
+            <label>
+              <input type="checkbox" name="sfg" />
+              <div>Mantenha-me conectado</div>
+            </label>
+            <div className="password">
+              <a href="https://github.com/Luizrtr/">
+                <strong>Esqueceu minha senha?</strong>
+              </a>
             </div>
-        </ContainerLogin>
-    )
-}
+          </div>
+          <div>
+            <Button tw="w-full md:w-5/12 text-center" type="submit">
+              Entrar
+            </Button>
+          </div>
+        </form>
+      </div>
+    </ContainerLogin>
+  );
+};
 
 export default Login;
--- src/pages/NewUser/index.tsx
+++ src/pages/NewUser/index.tsx
@@ -1,15 +1,84 @@
-import React from "react"
+import React from 'react';
+import { useForm } from 'react-hook-form';
+import 'twin.macro';
 
-import {ContainerNewUser} from './styles'
+import { ContainerNewUser } from './styles';
+import image from '../../assets/Login/login.png';
+import Button from '../../components/Button';
 
-export const NewUser:React.FC = () => {
-    return(
-        <ContainerNewUser>
-            <div>
-                
-            </div>
-        </ContainerNewUser>
-    )
+interface ICreate {
+  name: string;
+  user: string;
+  email: string;
+  password: string;
 }
+
+export const NewUser: React.FC = () => {
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = async (inputData: ICreate) => {
+    const data = {
+      name: inputData.name,
+      user: inputData.user,
+      email: inputData.email,
+      password: inputData.password,
+    };
+    console.log(data);
+  };
+
+  return (
+    <ContainerNewUser>
+      <img src={image} alt="" />
+      <div className="content">
+        <h1>
+          Faça o cadastro do seu <span>usuário</span>
+        </h1>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="boxInput">
+            <label htmlFor="">Nome completo</label>
+            <input
+              id="name"
+              type="text"
+              placeholder="Digite o seu nome completo"
+              {...register('name', { required: true })}
+            />
+          </div>
+          <div className="boxInput">
+            <label htmlFor="">Usuário</label>
+            <input
+              id="user"
+              type="text"
+              placeholder="Digite o seu usuário"
+              {...register('user', { required: true })}
+            />
+          </div>
+          <div className="boxInput">
+            <label htmlFor="">E-mail</label>
+            <input
+              id="email"
+              type="text"
+              placeholder="Digite o seu e-mail"
+              {...register('email', { required: true })}
+            />
+          </div>
+          <div className="boxInput">
+            <label htmlFor="">Senha</label>
+            <input
+              id="password"
+              type="password"
+              placeholder="Digite a sua senha"
+              {...register('password', { required: true })}
+            />
+          </div>
+          <div>
+            <Button tw="w-full md:w-5/12 text-center" type="submit">
+              Salvar
+            </Button>
+          </div>
+        </form>
+      </div>
+    </ContainerNewUser>
+  );
+};
 
 export default NewUser;
